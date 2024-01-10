3d37ea573014e33a99922c20053870e884556066
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-10 13:54:20 +0100

    Add reporter page

---
+++ Pin.LiveSports.Blazor/Pages/Reporter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pin.LiveSports.Blazor.Pages
+{
+    public class ReporterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
