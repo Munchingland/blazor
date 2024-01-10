44551d46043a72da2022456ef84c1682b2d4ce47
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-10 15:43:11 +0100

    Rename send method

--- Pin.LiveSports.Blazor/Hubs/MatchHub.cs
+++ Pin.LiveSports.Blazor/Hubs/MatchHub.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Pin.LiveSports.Blazor.Models;
 
 namespace Pin.LiveSports.Blazor.Hubs
 {
     public class MatchHub : Hub
     {
-        public async Task Send(string update)
+        public async Task SendUpdate(MatchUpdate update)
         {
             await Clients.All.SendAsync("matchUpdate", update);
         }
