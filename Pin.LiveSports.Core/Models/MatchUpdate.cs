1d0c31020a1c33b7871e34f78a44bf926251a9f1
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-10 19:54:11 +0100

    Move model to core

---
+++ Pin.LiveSports.Core/Models/MatchUpdate.cs
@@ -0,0 +1,27 @@
+﻿using Pin.LiveSports.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+
+    public class MatchUpdate
+    {
+        [Required(ErrorMessage = "gelieve een update te geven")]
+        public string UpdateMessage { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string FullUpdate
+        {
+            get
+            {
+                return $"{TimeOnly.FromDateTime(Time)}: {UpdateMessage}";
+            }
+        }
+    }
+}
