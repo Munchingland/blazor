8e52018d43bcff6b6b30800ea68bc5c57886ac23
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-10 14:27:44 +0100

    Define matchHub

---
+++ Pin.LiveSports.Blazor/Hubs/MatchHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Pin.LiveSports.Blazor.Hubs
+{
+    public class MatchHub : Hub
+    {
+        public async Task Send(string update)
+        {
+            await Clients.All.SendAsync("matchUpdate", update);
+        }
+    }
+}
