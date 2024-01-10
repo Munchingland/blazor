d2fed56f8fdca91d10abaa916c203d8b79c701d0
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-10 19:53:49 +0100

    Make a matchService in core

---
+++ Pin.LiveSports.Core/Services/Interfaces/IMatchService.cs
@@ -0,0 +1,15 @@
+﻿using Pin.LiveSports.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Services.Interfaces
+{
+    public interface IMatchService
+    {
+        public List<MatchUpdate> GetMatchHistory();
+        public void AddUpdateToHistory(MatchUpdate update);
+    }
+}
---
+++ Pin.LiveSports.Core/Services/MatchService.cs
@@ -0,0 +1,24 @@
+﻿using Pin.LiveSports.Core.Models;
+using Pin.LiveSports.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Services
+{
+    public class MatchService : IMatchService
+    {
+        private static List<MatchUpdate> matchHistory = new List<MatchUpdate>();
+        public List<MatchUpdate> GetMatchHistory()
+        {
+            return matchHistory;
+        }
+
+        public void AddUpdateToHistory(MatchUpdate update)
+        {
+            matchHistory.Add(update);
+        }
+    }
+}
