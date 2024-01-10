3404c0a0fab81a8a8e7d9aa8ef8b2d3eebd1bd1b
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-10 14:27:52 +0100

    Add matchUpdate model

---
+++ Pin.LiveSports.Blazor/Models/MatchUpdate.cs
@@ -0,0 +1,17 @@
+﻿namespace Pin.LiveSports.Blazor.Models
+{
+    public class MatchUpdate
+    {
+        public string UpdateMessage { get; set; }
+
+        public TimeOnly Time { get; set; }
+
+        public string Update 
+        { 
+            get
+            {
+                return $"{Time}: {UpdateMessage}";
+            }
+        }
+    }
+}
