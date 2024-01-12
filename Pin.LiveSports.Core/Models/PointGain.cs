2dafd3d3478764645bddf49334eb6b9ca30aae0f
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:28:11 +0100

    Add pointgain model

---
+++ Pin.LiveSports.Core/Models/PointGain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class PointGain
+    {
+        public int StyleAndFluidity { get; set; }
+        public int VarietyOfTricks { get; set; }
+        public int DifficultyOfManuevers { get; set; }
+        public int HeightAndAirTime { get; set; }
+        public int InnovationAndCreativity { get; set; }
+    }
+}
