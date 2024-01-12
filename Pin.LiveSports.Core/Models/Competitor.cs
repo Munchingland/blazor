38ca39543b81987e53c50d0ef14d934efbec0b55
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 15:41:18 +0100

    Add id to competitor

--- Pin.LiveSports.Core/Models/Competitor.cs
+++ Pin.LiveSports.Core/Models/Competitor.cs
@@ -4,6 +4,7 @@ namespace Pin.LiveSports.Core.Models
 {
     public class Competitor
     {
+        public int Id {  get; set; }
         public WindSurfer Surfer { get; set; }
         public PointGain? PointsGained { get; set; }
         public bool WonCompetition { get; set; }
