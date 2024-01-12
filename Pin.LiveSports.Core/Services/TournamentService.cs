377a988ee94eb91c320ad08431fa485ea395af8a
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:28:59 +0100

    Add tournament service and interface

---
+++ Pin.LiveSports.Core/Services/TournamentService.cs
@@ -0,0 +1,15 @@
+﻿using Pin.LiveSports.Core.Models;
+using Pin.LiveSports.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Services
+{
+    public class TournamentService : ITournamentService
+    {
+        private static List<Tournament> _tournaments = new List<Tournament>();
+    }
+}
