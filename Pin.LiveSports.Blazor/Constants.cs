71244699f53d1311042dfc156fdcd9756807aa88
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:47:28 +0100

    Add constants

--- Pin.LiveSports.Blazor/Constants.cs
+++ Pin.LiveSports.Blazor/Constants.cs
@@ -3,5 +3,8 @@
     public static class Constants
     {
         public const string MatchHubUrl = "/matchHub";
+
+        public const string Competitor = "deelnemer";
+        public const string Tournament = "tournooi";
     }
 }
