fdc318dac491d3357227833368a0395a833c3b2d
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 15:04:11 +0100

    Create base tournament and interface

--- Pin.LiveSports.Core/Services/Interfaces/ITournamentService.cs
+++ Pin.LiveSports.Core/Services/Interfaces/ITournamentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pin.LiveSports.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Pin.LiveSports.Core.Services.Interfaces
 {
     public interface ITournamentService
     {
+
+        void CreateTournament(List<Competitor> competitors, string name);
+
+        List<Tournament> GetAll();
     }
 }
--- Pin.LiveSports.Core/Services/TournamentService.cs
+++ Pin.LiveSports.Core/Services/TournamentService.cs
@@ -10,6 +10,31 @@ namespace Pin.LiveSports.Core.Services
 {
     public class TournamentService : ITournamentService
     {
-        private static List<Tournament> _tournaments = new List<Tournament>();
+        private static List<Tournament> _tournaments;
+        public TournamentService()
+        {
+            _tournaments = new List<Tournament>();
+        }
+
+
+        public void CreateTournament(List<Competitor> competitors, string name)
+        {
+            int id = _tournaments.Select(a => a.Id).DefaultIfEmpty(0).Max();
+            foreach (var competitor in competitors)
+            {
+
+            }
+            Tournament toAdd = new Tournament
+            {
+                Competitors = competitors,
+                Name = name,
+                Id = id
+            };
+        }
+
+        public List<Tournament> GetAll()
+        {
+            return _tournaments;
+        }
     }
 }
