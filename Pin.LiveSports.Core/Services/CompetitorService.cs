ef647133af2956ff3f8fe0eaf7ae7dfaa000baf9
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:42:13 +0100

    Create base for service and implement interface

--- Pin.LiveSports.Core/Services/CompetitorService.cs
+++ Pin.LiveSports.Core/Services/CompetitorService.cs
@@ -1,4 +1,5 @@
-﻿using Pin.LiveSports.Core.Services.Interfaces;
+﻿using Pin.LiveSports.Core.Models;
+using Pin.LiveSports.Core.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,39 @@ namespace Pin.LiveSports.Core.Services
 {
     public class CompetitorService : ICompetitorService
     {
+        private Random random = new Random();
+        private static List<Competitor> _competitors;
 
+        public CompetitorService()
+        {
+            _competitors = new List<Competitor>();
+            CreateRandomCompetitors(10);
+
+        }
+
+        private void CreateRandomCompetitors(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Competitor competitor = new Competitor
+                {
+                    Name =$"Deelnemer{i}",
+                    Id = i ,
+                };
+                _competitors.Add(competitor);
+            }
+        }
+
+
+        public void AddCompetitor(Competitor competitor)
+        {
+            competitor.Id = _competitors.Max(c=>c.Id) + 1;
+            _competitors.Add(competitor);
+        }
+
+        public List<Competitor> GetAll()
+        {
+            return _competitors;
+        }
     }
 }
--- Pin.LiveSports.Core/Services/Interfaces/ICompetitorService.cs
+++ Pin.LiveSports.Core/Services/Interfaces/ICompetitorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pin.LiveSports.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Pin.LiveSports.Core.Services.Interfaces
 {
     public interface ICompetitorService
     {
+        void AddCompetitor(Competitor competitor);
+        List<Competitor> GetAll();
     }
 }
