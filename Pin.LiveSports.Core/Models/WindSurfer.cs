825251361666fc11b4748c3a7d8dc701870a2042
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 17:32:22 +0100

    Add required to name

--- Pin.LiveSports.Core/Models/WindSurfer.cs
+++ Pin.LiveSports.Core/Models/WindSurfer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Pin.LiveSports.Core.Models
     public class WindSurfer
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Gelieve een naam in te geven")]
         public string Name { get; set; }
     }
 }
