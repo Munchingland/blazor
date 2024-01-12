ea633dd16e8f062cc229a2e093d9eafc17a68b33
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:28:45 +0100

    Add competitorService and interface

---
+++ Pin.LiveSports.Core/Services/CompetitorService.cs
@@ -0,0 +1,14 @@
+﻿using Pin.LiveSports.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Services
+{
+    public class CompetitorService : ICompetitorService
+    {
+
+    }
+}
