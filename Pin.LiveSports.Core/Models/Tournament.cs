aa3b61756ed500eadb3ef02ab52b2839ae729a89
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:28:20 +0100

    Add tournament model

---
+++ Pin.LiveSports.Core/Models/Tournament.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class Tournament
+    {
+        public List<Competitor> Competitors { get; set; }
+        public List<MatchUpdate> MatchHistory { get; set; }
+        public string NameCompetition { get; set; }
+
+    }
+}
