e2be0e94144ac4e105f4a1d5a2852a76b57da9d5
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-12 14:28:02 +0100

    Add competitor model

---
+++ Pin.LiveSports.Core/Models/Competitor.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class Competitor
+    {
+        public string Name { get; set; }
+        public PointGain PointsGained { get; set; } 
+        public bool WonCompetition { get; set; }
+    }
+}
