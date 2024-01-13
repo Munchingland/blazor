c1e28e5374955c6151be0979faaf5e493231b379
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-13 23:29:48 +0100

    Make int nullable

--- Pin.LiveSports.Core/Models/PointGain.cs
+++ Pin.LiveSports.Core/Models/PointGain.cs
@@ -8,12 +8,12 @@ namespace Pin.LiveSports.Core.Models
 {
     public class PointGain
     {
-        public int StyleAndFluidity { get; set; }
-        public int VarietyOfTricks { get; set; }
-        public int DifficultyOfManuevers { get; set; }
-        public int HeightAndAirTime { get; set; }
-        public int InnovationAndCreativity { get; set; }
+        public int? StyleAndFluidity { get; set; }
+        public int? VarietyOfTricks { get; set; }
+        public int? DifficultyOfManuevers { get; set; }
+        public int? HeightAndAirTime { get; set; }
+        public int? InnovationAndCreativity { get; set; }
         public string Phase { get; set; }
-        public int TotalPoints { get; set; }
+        public int? TotalPoints { get; set; }
     }
 }
