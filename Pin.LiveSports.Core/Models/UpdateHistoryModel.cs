09ddd0a39ffd27c5c4b02af1dcdfac851f34bdc9
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-13 23:30:46 +0100

    New model for update history

---
+++ Pin.LiveSports.Core/Models/UpdateHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class UpdateHistoryModel
+    {
+        public PointGain PointGain { get; set; }
+
+        public Tournament TournamentToUpdateHistory { get; set; }
+
+        public MatchUpdate MatchUpdate { get; set; }
+
+        public Competitor Competitor { get; set; }
+    }
+}
