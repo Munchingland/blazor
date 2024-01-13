72849b874979427eb7b2b6e06033743735cac49f
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-13 23:30:18 +0100

    Add constant for start

--- Pin.LiveSports.Core/Constants.cs
+++ Pin.LiveSports.Core/Constants.cs
@@ -8,12 +8,14 @@
         public const string Tournament = "tournooi";
 
 
-        public const string Intro = "inleidende beweging";
+        public const string Intro = "Inleidende beweging";
         public const string Single = "Enkele beweging";
-        public const string Combo = "combinatie bewegingen";
+        public const string Combo = "Combinatie bewegingen";
         public const string SwitchStance = "Verander houding";
-        public const string Air = "lucht bewegingen";
+        public const string Air = "Lucht bewegingen";
         public const string Power = "Hoge energie bewegingen";
         public const string Final = "Laatste beweging";
+
+        public const string Start = "start";
     }
 }
