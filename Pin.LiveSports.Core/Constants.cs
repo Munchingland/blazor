a473ad1855119a3ea970aa1355979d1ed8bec9d7
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-13 15:57:33 +0100

    Move constants to core

---
+++ Pin.LiveSports.Core/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace Pin.LiveSports.Core
+{
+    public static class Constants
+    {
+        public const string MatchHubUrl = "/matchHub";
+
+        public const string Competitor = "deelnemer";
+        public const string Tournament = "tournooi";
+
+
+        public const string Intro = "inleidende beweging";
+        public const string Single = "Enkele beweging";
+        public const string Combo = "combinatie bewegingen";
+        public const string SwitchStance = "Verander houding";
+        public const string Air = "lucht bewegingen";
+        public const string Power = "Hoge energie bewegingen";
+        public const string Final = "Laatste beweging";
+    }
+}
