bfd79bc8585b3e6297fb6c657eef342a26ab0a57
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-14 16:36:40 +0100

    Add data annotation to pointgain

--- Pin.LiveSports.Core/Models/PointGain.cs
+++ Pin.LiveSports.Core/Models/PointGain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,18 @@ namespace Pin.LiveSports.Core.Models
 {
     public class PointGain
     {
+        [Required]
         public int? StyleAndFluidity { get; set; }
+        [Required]
         public int? VarietyOfTricks { get; set; }
+        [Required]
         public int? DifficultyOfManuevers { get; set; }
+        [Required]
         public int? HeightAndAirTime { get; set; }
+        [Required]
         public int? InnovationAndCreativity { get; set; }
         public string Phase { get; set; }
         public int? TotalPoints { get; set; }
+        public double? AveragePoints { get; set; }
     }
 }
