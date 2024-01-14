73b62b8cd1965c28f31c00a85c148ff99b5b6c94
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-14 16:36:57 +0100

    Show total points competitor has gained

--- Pin.LiveSports.Core/Models/Competitor.cs
+++ Pin.LiveSports.Core/Models/Competitor.cs
@@ -8,5 +8,8 @@ namespace Pin.LiveSports.Core.Models
         public WindSurfer Surfer { get; set; }
         public List<PointGain>? PointsGained { get; set; }
         public bool WonCompetition { get; set; }
+        public bool FinishedRoutine { get; set; }
+
+        public int? TotalPointsGained => PointsGained.Sum(p => p.TotalPoints);
     }
 }
