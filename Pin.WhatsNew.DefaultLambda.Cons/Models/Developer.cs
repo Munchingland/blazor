44e6903544373b1032b7ec75419d0caf75eb7912
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-16 20:04:44 +0100

    Add models for demo

---
+++ Pin.WhatsNew.DefaultLambda.Cons/Models/Developer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.WhatsNew.DefaultLambda.Cons.Models
+{
+    internal class Developer : BaseModel
+    {
+    }
+}
